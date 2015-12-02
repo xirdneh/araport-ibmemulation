dbcefaffb89c221bdb28bc440633dfe263f38577
Josue Balandrano Coronel <xirdneh@gmail.com>
2015-12-02 10:56:27 -0600

    Modified the way js scripts are loaded

---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+/* globals window, AIP, $ */
+  'use strict';
+  window.addEventListener('Agave::ready', function() {
+    if(console){
+        console.log('Agave has been initialized, do something');
+        /* Remove this if you don't want to print the example */
+          var appContext = $('[data-app-name="ibm-emulation"]');
+          AIP.displayDocs(appContext);
+          //AIP.displayList(appContext, 'appNamespace', 'appService', {});
+    }
+  });
---
+++ app/scripts/pcjs/runner.js
@@ -0,0 +1,1 @@
+window.embedPC('ibm5150', 'app/assets/machine.xml', 'app/assets/components.xsl');
